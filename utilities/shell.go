06ff2760e370c0829d41d2dc3e6409a248c0a1e3
jerry-enebeli <>
2020-02-28 01:31:57 +0100

    create shell uitility function

---
+++ utilities/shell.go
@@ -0,0 +1,17 @@
+package utilities
+
+import (
+	"bytes"
+	"os/exec"
+)
+
+// Shell runs shell commands and return the output of the command and the command error
+func Shell(shell, command string) (string, string, error) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command(shell, "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return stdout.String(), stderr.String(), err
+}
