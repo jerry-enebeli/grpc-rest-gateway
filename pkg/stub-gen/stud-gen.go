98944997b06d29e53bad5357b6b3ece0f3a1b22e
jerry <jerryenebeli@icloud.com>
2020-09-02 07:22:19 +0100

    update package name

---
+++ pkg/stub-gen/stud-gen.go
@@ -0,0 +1,35 @@
+package stubgen
+
+import (
+	"fmt"
+	"github.com/jerry-enebeli/grpc-rest-gateway/tools"
+	"log"
+	"path"
+)
+
+//GenerateStub generate gRPC stubs based on provided protobuf file
+//it saves all generated code in /proto-clients dir
+func GenerateStub(protofile, output string) error {
+
+	protofileDir := path.Dir(protofile)
+	protoInputFile := path.Base(protofile)
+
+	//create proto outout dir
+	protoCompilerCommand := fmt.Sprintf("protoc --proto_path=%s/ %s  --go_out=plugins=grpc:%s", protofileDir, protoInputFile, output)
+
+	log.Println("compiling proto file......", protoCompilerCommand)
+	//runs protobuf compiler on a bah shell
+	_, cmdError, err := tools.Shell("bash", protoCompilerCommand)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if cmdError != "" {
+		log.Fatalf("can not compile proto file: %s", cmdError)
+	}
+
+	log.Println("proto file compiled, check the dir proto-clients for generated stubs")
+
+	return nil
+}
