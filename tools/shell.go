d6af7b57c57520b4ba79a94b926816b023dfd517
jerry <jerryenebeli@icloud.com>
2020-09-06 05:58:10 +0100

    update shell function to return stdout and error.

--- tools/shell.go
+++ tools/shell.go
@@ -6,12 +6,13 @@ import (
 )
 
 // Shell runs shell commands and return the output of the command and the command error
-func Shell(shell, command string) (string, string, error) {
+func Shell(shell, command string) (output string, err error) {
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 	cmd := exec.Command(shell, "-c", command)
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
-	err := cmd.Run()
-	return stdout.String(), stderr.String(), err
+	err = cmd.Run()
+
+	return stdout.String(), err
 }
