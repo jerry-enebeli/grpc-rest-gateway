4ee4fba8437c3c8e0e7191779676edc643d6d4c8
jerry <jerryenebeli@icloud.com>
2020-09-21 03:15:37 +0100

    create example server

---
+++ examples/greeter_server/main.go
@@ -0,0 +1,47 @@
+//go:generate protoc -I ../helloworld --go_out=plugins=grpc:../helloworld ../helloworld/helloworld.proto
+
+// Package main implements a server for Greeter service.
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	_ "github.com/jerry-enebeli/grpc-rest-gateway/codec"
+
+	pb "github.com/jerry-enebeli/grpc-rest-gateway/examples/helloworld"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	fmt.Println("server started at " + port)
+	s := grpc.NewServer()
+
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
